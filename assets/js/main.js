b7178a2b196fb07ba867320a78d6044165770dd2
ansolvera <38958444+ansolvera@users.noreply.github.com>
2021-08-18 12:43:46 -0700

    Fix email forms

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,8 @@ window.addEventListener('load', () => {
     if(!shownModal) {
         setTimeout(() => {
             modal.classList.add('is-revealed');
-            shownModal = true;
-        }, 30000)
+            //shownModal = true;
+        }, 1000)
     }
 });
 
@@ -34,3 +34,43 @@ window.addEventListener('scroll', () => {
         nav.classList.remove('black');
     }
 });
+
+document.querySelector('#sib-form').addEventListener('submit', () => {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'https://c23ef8a2.sibforms.com/serve/MUIEAH0VpauvuLzv9DvebiNNmcFnbMuHYabHk3EC0Y2Le5J5Lr160C3MHWvSgZUiNukCOjbxUOg5AkuJ1WUv2KhYdb2A3N9ycAax5slE74GgbQn_iYcGVWnSkhvNQNaZyz5UFZ0Lpow82tPLTIl3EiTVKJcehGOjA-vKMEomNFY8FKj-SNXir83YZTNJ5QAEJYk5JD2a9eJGO_w4', true)
+
+    xhr.onload = function() {
+        let success = document.querySelector('#success-message');
+        let error = document.querySelector('#error-message');
+
+        if(xhr.status == 200) {
+            success.classList.add('is-shown')
+        } else {
+            error.classList.add('is-shown')
+        } return false;
+    }
+
+    xhr.send();
+
+})
+
+document.querySelector('#sib-form--modal').addEventListener('submit', () => {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('POST', 'https://c23ef8a2.sibforms.com/serve/MUIEAH0VpauvuLzv9DvebiNNmcFnbMuHYabHk3EC0Y2Le5J5Lr160C3MHWvSgZUiNukCOjbxUOg5AkuJ1WUv2KhYdb2A3N9ycAax5slE74GgbQn_iYcGVWnSkhvNQNaZyz5UFZ0Lpow82tPLTIl3EiTVKJcehGOjA-vKMEomNFY8FKj-SNXir83YZTNJ5QAEJYk5JD2a9eJGO_w4', true)
+
+    xhr.onload = function() {
+        let modalSuccess = document.querySelector('#success-message--modal');
+        let modalError = document.querySelector('#error-message--modal');
+
+        if(xhr.status == 200) {
+            modalSuccess.classList.add('is-shown')
+        } else {
+            modalError.classList.add('is-shown')
+        } return false;
+    }
+
+    xhr.send();
+
+})
